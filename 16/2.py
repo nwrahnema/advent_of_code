1b6f886d60c6aa158762de2163795e21cdac6d08
Nima Rahnema <nwrahnema@gmail.com>
2022-01-03 22:33:13 -0500

    16.2

--- 16/2.py
+++ 16/2.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from itertools import islice
 from typing import Iterator
+from math import prod
 
 
 class StringReader:
@@ -16,14 +17,14 @@ class StringReader:
 
 @dataclass
 class Result:
-    version: int
+    value: int
     bits_read: int
 
 
 def decode(packet: Iterator[str]) -> Result:
     string_reader = StringReader(packet)
 
-    version_total = int(string_reader.take(3), 2)
+    string_reader.take(3)  # version number
     type_id = int(string_reader.take(3), 2)
     if type_id == 4:
         literal_value = ""
@@ -33,29 +34,52 @@ def decode(packet: Iterator[str]) -> Result:
             is_last = string_reader.take(1) == "0"
             literal_value += string_reader.take(4)
 
-        return Result(version_total, string_reader.count)
+        return Result(int(literal_value, 2), string_reader.count)
     else:
         length_type_id = int(string_reader.take(1), 2)
         subpacket_bits_read = 0
+        subpacket_values = []
 
         if length_type_id == 0:
             length = int(string_reader.take(15), 2)
 
             while subpacket_bits_read < length:
                 result = decode(packet)
-                version_total += result.version
+                subpacket_values.append(result.value)
                 subpacket_bits_read += result.bits_read
         elif length_type_id == 1:
             num_subpackets = int(string_reader.take(11), 2)
 
             for _ in range(num_subpackets):
                 result = decode(packet)
-                version_total += result.version
+                subpacket_values.append(result.value)
                 subpacket_bits_read += result.bits_read
         else:
             raise Exception(f"Invalid length type ID: {length_type_id}")
 
-        return Result(version_total, string_reader.count + subpacket_bits_read)
+        value = None
+        match type_id:
+            case 0:
+                value = sum(subpacket_values)
+            case 1:
+                value = prod(subpacket_values)
+            case 2:
+                value = min(subpacket_values)
+            case 3:
+                value = max(subpacket_values)
+            case 5:
+                first, second = subpacket_values
+                value = int(first > second)
+            case 6:
+                first, second = subpacket_values
+                value = int(first < second)
+            case 7:
+                first, second = subpacket_values
+                value = int(first == second)
+            case _:
+                raise Exception(f"Invalid type ID: {type_id}")
+
+        return Result(value, string_reader.count + subpacket_bits_read)
 
 
 with open("input.txt") as file:
@@ -66,4 +90,4 @@ packet = f"{int('1' + packet, 16):b}"[1:]
 
 result = decode(iter(packet))
 
-print(result.version)
+print(result.value)
