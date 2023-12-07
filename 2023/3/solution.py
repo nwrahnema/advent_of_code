c9dad50a4b8a619c32643cf71195d17385cc9641
Nima Rahnema <nwrahnema@gmail.com>
2023-12-06 21:43:16 -0500

    2023q3.2

--- 2023/3/solution.py
+++ 2023/3/solution.py
@@ -3,7 +3,7 @@ from pathlib import Path
 with open(Path(__file__).with_name("input.txt")) as f:
     grid = f.read().splitlines()
 
-total = 0
+star_counts = [[[] for _ in grid[0]] for _ in grid]
 
 i = 0
 while i < len(grid):
@@ -11,7 +11,7 @@ while i < len(grid):
     while j < len(grid[i]):
         if grid[i][j].isdigit():
             number = []
-            found = False
+            found = set()
             while j < len(grid[i]) and grid[i][j].isdigit():
                 number += grid[i][j]
                 for x in [
@@ -22,12 +22,12 @@ while i < len(grid):
                         for coord in [j - 1, j, j + 1]
                         if 0 <= coord < len(grid[x])
                     ]:
-                        if not grid[x][y].isalnum() and grid[x][y] != ".":
-                            found = True
+                        if grid[x][y] == "*":
+                            found.add((x, y))
                 j += 1
-            if found:
-                total += int("".join(number))
+            for x, y in found:
+                star_counts[x][y].append(int("".join(number)))
         j += 1
     i += 1
 
-print(total)
+print(sum(lst[0] * lst[1] for row in star_counts for lst in row if len(lst) == 2))
